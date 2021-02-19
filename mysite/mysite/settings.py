761d221eb77e9e980f9f81886a576e28afd9491a
Александр <pustowoytenko@gmail.com>
2021-02-19 17:24:45 +0700

    Feed added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
     'taggit',
 ]
